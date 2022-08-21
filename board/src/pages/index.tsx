23a587bebc31fb036407678a1550e32ecb5d8567
Tiago Abreu <devtiagoabreu@gmail.com>
2022-08-21 08:27:23 -0300

    Criando o Título dinâmico

--- board/src/pages/index.tsx
+++ board/src/pages/index.tsx
@@ -1,9 +1,15 @@
-import styles from '../styles/styles.module.scss'
+import Head from '../../node_modules/next/head';
+import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Board de Conferências</title>
+    </Head>    
     <div>
       <h1 className={styles.title}>Board <span>wow</span></h1>
     </div>
+    </>
   )
 }
