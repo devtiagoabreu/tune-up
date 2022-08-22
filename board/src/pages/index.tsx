e0e8c64f3bd2d3d168219ce5af1716eb2a3f506b
Tiago Abreu <devtiagoabreu@gmail.com>
2022-08-21 21:51:54 -0300

    Criando a Home

--- board/src/pages/index.tsx
+++ board/src/pages/index.tsx
@@ -7,9 +7,25 @@ export default function Home() {
     <Head>
       <title>Board de Conferências</title>
     </Head>    
-    <div>
-      <h1 className={styles.title}>Board <span>wow</span></h1>
-    </div>
+    <main className={styles.contentContainer}>
+      <img src="/images/board-user.svg" alt="Ferramenta board" />
+      
+      <section className={styles.callToAction}>
+        <h1>
+          Conferência de Entrada de Matéria Prima
+        </h1>
+        <p>
+          <span>Integrado com </span>
+          ERP Systêxtil.
+        </p>
+      </section>
+
+      <div className={styles.donaters}>
+        <img src="https://avatars.githubusercontent.com/u/9002790?v=4" alt="Tiago de Abreu" />
+        <img src="https://avatars.githubusercontent.com/u/104369571?v=4" alt="Rodrigo Moura" />
+        <img src="https://avatars.githubusercontent.com/u/85912921?v=4" alt="Igor Santiéllo" />
+      </div>
+    </main>
     </>
   )
 }
